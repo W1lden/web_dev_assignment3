7ff579bea1fa0ed2f67e6a6f0cede56f19e7c661
Асанали Битанов <asan@MacBook-Air-Asanali.local>
2024-11-06 17:11:14 +0500

    delete button

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, create_post
+from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', create_post, name='create_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 class PostListView(ListView):
     model = Post
@@ -25,3 +25,10 @@ def create_post(request):
     else:
         form = PostForm()
     return render(request, 'blog/create_post.html', {'form': form})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/delete_post.html', {'post': post})
